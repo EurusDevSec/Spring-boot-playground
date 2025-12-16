a821fe9e41de644d19de1a7d90088c8b71444b90
LeganSar44 <hoangslevan@gmail.com>
2025-12-16 23:46:55 +0700

    initialize Spring Boot project with basic structure and HelloController

---
+++ src/main/java/com/eurus/hello_spring_boot/HelloController.java
@@ -0,0 +1,13 @@
+package com.eurus.hello_spring_boot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello World! I will be a Devops Engineer.";
+
+    }
+}
