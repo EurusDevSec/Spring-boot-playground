b7e1e31ea24f5abbe45464db07fb31a7898faffb
LeganSar44 <hoangslevan@gmail.com>
2025-12-17 16:45:25 +0700

    feat: Add API test cases for user creation and retrieval; fix method name in UserRepository

--- Identity-Service/src/main/java/com/eurus/Identity_Service/repository/UserRepository.java
+++ Identity-Service/src/main/java/com/eurus/Identity_Service/repository/UserRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-    boolean existByUsername(String username);
+    boolean existsByUsername(String username);
 
 }
