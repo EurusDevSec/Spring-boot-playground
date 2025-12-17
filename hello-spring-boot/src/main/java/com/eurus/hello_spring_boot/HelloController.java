b8b64ba4eb6012d7a81b7d5d8b30d22ba7630076
LeganSar44 <hoangslevan@gmail.com>
2025-12-17 10:21:29 +0700

    Refactor: Move project hello-spring-boot to sub-folder

--- hello-spring-boot/src/main/java/com/eurus/hello_spring_boot/HelloController.java
+++ hello-spring-boot/src/main/java/com/eurus/hello_spring_boot/HelloController.java
@@ -1,13 +1,14 @@
 package com.eurus.hello_spring_boot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-    public String sayHello(){
-        return "Hello World! I will be a Devops Engineer.";
+    public String sayHello(@RequestParam(value = "name", defaultValue = "World") String name){
+        return "Hello " + name + "! Toi se tro thanh DevOps Engineer!";
 
     }
     @GetMapping("Goodbye")
