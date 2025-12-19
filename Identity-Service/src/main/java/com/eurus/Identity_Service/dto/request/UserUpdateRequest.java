13522736f609301f349151d66cb9d7fccd169352
LeganSar44 <hoangslevan@gmail.com>
2025-12-19 13:18:59 +0700

    feat: add UserUpdateRequest.java (DTO) for update

--- Identity-Service/src/main/java/com/eurus/Identity_Service/dto/request/UserUpdateRequest.java
+++ Identity-Service/src/main/java/com/eurus/Identity_Service/dto/request/UserUpdateRequest.java
@@ -1,4 +1,21 @@
 package com.eurus.Identity_Service.dto.request;
 
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
+
 public class UserUpdateRequest {
+    String password;
+    String firstName;
+    String lastName;
+    LocalDate dob;
 }
