bacb20615d0ab2b869e887126eb38b9a36db324a
LeganSar44 <hoangslevan@gmail.com>
2025-12-19 15:59:42 +0700

    feat: add validation constraints for username and password in UserCreationRequest

--- Identity-Service/src/main/java/com/eurus/Identity_Service/dto/request/UserCreationRequest.java
+++ Identity-Service/src/main/java/com/eurus/Identity_Service/dto/request/UserCreationRequest.java
@@ -1,6 +1,7 @@
 package com.eurus.Identity_Service.dto.request;
 
 
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -16,7 +17,9 @@ import java.time.LocalDate;
 public class UserCreationRequest {
 
     //Khong co Id vi ID tu generate
+    @Size(min=3,message = "Username must be at least 3 characters")
     String username;
+    @Size(min=8,message = "Password must be at least 8 characters")
     String password;
     String firstName;
     String lastName;
